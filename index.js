10acd5c4869233a4bdfcfa2d2ec4d7f84be3426b
corporateanon <corporateanon@gmail.com>
2015-01-10 21:12:57 +0200

    view assets, simple data cache

--- index.js
+++ index.js
@@ -2,12 +2,30 @@
   var apiUrl = 'http://unthought.net/jobapi/';
   var defaultFolder = 'root';
 
+  var Cache = function() {
+    function Cache(ttl) {
+      this.ttl = ttl;
+      this.items = {};
+    }
+    Cache.prototype.set = function(key, value) {
+      this.items[key] = [Date.now() + this.ttl, value];
+    };
+    Cache.prototype.get = function(key) {
+      var record = this.items[key];
+      if (record && record[0] > Date.now()) {
+        return record[1];
+      }
+    };
+    return Cache;
+  }();
+
   var FoldersListView = function() {
     function FoldersListView(el) {
       this.el = el;
+      this.assets = build(el);
       attachEvents(this);
     }
-    FoldersListView.prototype._onClick = function(event) {
+    FoldersListView.prototype._onListItemClick = function(event) {
       var t = event.target;
       var id = t.getAttribute('data-id');
       notify(this, 'navigate', [id]);
@@ -15,37 +33,60 @@
 
     FoldersListView.prototype.setItems = function(items) {
       this.items = items;
-    }
+    };
+    FoldersListView.prototype.setPage = function(page) {
+      this.page = page;
+    };
+
     FoldersListView.prototype.render = function() {
-      var html = this.items.map(function(item) {
+      debugger
+      console.time('build html');
+      var html = this.items.slice(0, 10).map(function(item) {
         return '<li class="folder" data-id="' + item.id + '">' + item.name + '</li>';
       }).join('\n');
-      this.el.innerHTML = html;
+      console.timeEnd('build html');
+      console.time('set html');
+      this.assets.list.innerHTML = html;
+      console.timeEnd('set html');
     };
 
     function attachEvents(view) {
-      view.el.addEventListener('click', view._onClick.bind(view));
+      view.assets.list.addEventListener('click', view._onListItemClick.bind(view));
     }
 
     function notify(view, signal, attrs) {
       view['on_' + signal].apply(view, attrs);
     }
 
+    function build(el) {
+      el.innerHTML = '<ul class="list"></ul><div class="pager"><a class="prev"></a><a class="next"></a><span class="page"></span></div>';
+      return {
+        list: el.querySelector('.list'),
+        pagerPage: el.querySelector('.pager > .page'),
+        pagerNext: el.querySelector('.pager > .next'),
+        pagerPrev: el.querySelector('.pager > .prev'),
+      }
+    }
+
     return FoldersListView;
   }();
 
   function parseFoldersXml(doc) {
-    return Array.prototype.slice.call(doc.querySelectorAll('folder'))
+    console.time('parse xml');
+    var items = Array.prototype.slice.call(doc.querySelectorAll('folder'))
       .map(function(folder) {
         return {
           id: folder.querySelector('id').textContent,
           name: folder.querySelector('name').textContent,
         }
       });
+    console.timeEnd('parse xml');
+    return items;
   }
 
-  function getFoldersXml(folder) {
-    return new Promise(function(resolve, reject) {
+  function getFoldersXml(folder, cache) {
+    var cached = cache.get(folder);
+    return cached ? Promise.resolve(cached) : new Promise(function(resolve, reject) {
       var url = [apiUrl, folder].join('/');
       var xhr = new XMLHttpRequest();
       xhr.onreadystatechange = function() {
@@ -59,20 +100,26 @@
       };
       xhr.open('GET', url, true);
       xhr.send(null);
-    }).then(parseFoldersXml);
+    }).then(parseFoldersXml).then(function(data) {
+      return cache.set(folder, data), data;
+    });
   }
 
   function main() {
+    var cache = new Cache(60000);
     var view = new FoldersListView(document.getElementById('folders'));
     var currentFolder = defaultFolder;
-    view.on_navigate = function (id) {
+    var currentPage = 0;
+    view.on_navigate = function(id) {
       currentFolder = id;
+      currentPage = 0;
       update();
     }
 
     function update() {
-      getFoldersXml(currentFolder).then(function(items) {
+      getFoldersXml(currentFolder, cache).then(function(items) {
         view.setItems(items);
+        view.setPage(currentPage);
         view.render();
       });
     }

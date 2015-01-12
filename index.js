119e3509336d30b3958e86b044c726c9aca046d3
corporateanon <corporateanon@gmail.com>
2015-01-12 23:10:44 +0200

    line count = 99

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-(function() {
-  var apiUrl = 'http://unthought.net/jobapi/';
-  var defaultFolder = 'root';
-  var itemsPerPage = 1000;
+(function(apiUrl, defaultFolder, itemsPerPage) {
   var Cache = function() {
     function Cache(ttl) {
       this.ttl = ttl;
@@ -12,58 +9,42 @@
     };
     Cache.prototype.get = function(key) {
       var record = this.items[key];
-      if (record && record[0] > Date.now()) {
-        return record[1];
-      }
+      return (record && record[0] > Date.now()) ? record[1] : undefined;
     };
     return Cache;
   }();
   var FoldersListView = function() {
     function FoldersListView(el) {
       this.el = el;
-      el.addEventListener('click', onClick.bind(this));
+      el.addEventListener('click', function(event) {
+        var data = event.target.dataset
+        data.action && this['on_' + data.action].apply(this, [data.value]);
+      }.bind(this));
       this.assets = build(el);
     }
 
-    function onClick(event) {
-      var t = event.target;
-      var action = t.getAttribute('data-action');
-      var value = t.getAttribute('data-value');
-      action && notify(this, action, [value]);
-    };
     FoldersListView.prototype.setState = function(state) {
       this.state = state;
     };
-    FoldersListView.prototype.render = function() {
-      console.time('build html');
-      var html = this.state.items.map(function(item) {
+    FoldersListView.prototype.render = function(assets, state) {
+      var html = (state = this.state).items.map(function(item) {
         return '<div class="folder" data-action="navigate" data-value="' + item.id + '">' + item.name + '</div>';
       }).join('');
-      this.assets.list.innerHTML = html;
-      this.assets.pagerPage.textContent = 'Page ' + (this.state.page + 1) + ' of ' + this.state.totalPages;
-      this.assets.pagerPage.style.display = this.state.totalPages > 1 ? 'inline-block' : 'none';
-      this.assets.pagerNext.style.display = this.state.page + 1 < this.state.totalPages ? 'inline-block' : 'none';
-      this.assets.pagerPrev.style.display = this.state.page > 0 ? 'inline-block' : 'none';
+      (assets = this.assets)[0].innerHTML = html;
+      assets[1].textContent = 'Page ' + (state.page + 1) + ' of ' + state.totalPages;
+      assets[1].style.display = state.totalPages > 1 ? 'inline-block' : 'none';
+      assets[2].style.display = state.page + 1 < state.totalPages ? 'inline-block' : 'none';
+      assets[3].style.display = state.page > 0 ? 'inline-block' : 'none';
     };
-
-    function notify(view, signal, attrs) {
-      view['on_' + signal].apply(view, attrs);
-    }
 
     function build(el) {
       el.innerHTML = document.querySelector('.template-folders-list-view').innerHTML;
-      return {
-        list: el.querySelector('.list'),
-        pagerPage: el.querySelector('.pager > .page'),
-        pagerNext: el.querySelector('.pager > .next'),
-        pagerPrev: el.querySelector('.pager > .prev'),
-      };
+      return [el.querySelector('.list'), el.querySelector('.pager > .page'), el.querySelector('.pager > .next'), el.querySelector('.pager > .prev')];
     }
     return FoldersListView;
   }();
 
   function parseFoldersXml(doc) {
-    console.time('parse xml');
     var items = Array.prototype.slice.call(doc.querySelectorAll('folder'))
       .map(function(folder) {
         return {
@@ -71,7 +52,6 @@
           name: folder.querySelector('name').textContent,
         }
       });
-    console.timeEnd('parse xml');
     return items;
   }
 
@@ -84,40 +64,36 @@
         if (xhr.readyState == 4) {
           xhr.status === 200 ? resolve(xhr.responseXML) : reject(new Error('HTTP ' + xhr.status));
         }
-      };
-      xhr.open('GET', url, true);
-      xhr.send(null);
+      }
+      xhr.open('GET', url, true) || xhr.send(null);
     }).then(parseFoldersXml).then(function(data) {
       return cache.set(folder, data), data;
     });
   }
 
-  function main() {
-    var cache = new Cache(60000);
-    var view = new FoldersListView(document.getElementById('folders'));
-    var currentFolder = defaultFolder;
-    var currentPage = 0;
-    view.on_navigate = function(id) {
-      currentFolder = id;
-      currentPage = 0;
-      update();
-    };
-    view.on_page = function(incr) {
-      currentPage = currentPage + parseInt(incr, 10);
-      update();
-    };
-
-    function update() {
-      getFoldersXml(currentFolder, cache).then(function(items) {
-        view.setState({
-          items: items.slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage),
-          page: currentPage,
-          totalPages: Math.ceil(items.length / itemsPerPage),
-        });
-        view.render();
-      });
-    }
+  var cache = new Cache(60000);
+  var view = new FoldersListView(document.getElementById('folders'));
+  var currentFolder = defaultFolder;
+  var currentPage = 0;
+  view.on_navigate = function(id) {
+    currentFolder = id;
+    currentPage = 0;
     update();
+  };
+  view.on_page = function(incr) {
+    currentPage = currentPage + parseInt(incr, 10);
+    update();
+  };
+
+  function update() {
+    getFoldersXml(currentFolder, cache).then(function(items) {
+      view.setState({
+        items: items.slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage),
+        page: currentPage,
+        totalPages: Math.ceil(items.length / itemsPerPage),
+      });
+      view.render();
+    });
   }
-  main();
-})();
+  update();
+})('http://unthought.net/jobapi/', 'root', 1000);
